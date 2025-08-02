c3730f351e19a5efa5cd0faa62adfb9f47bbf23e
alani25 <alanihamzah25@gmail.com>
2025-08-01 23:31:56 -0400

    added in line snapping

--- public/corelibs/wick-engine/wickengine.js
+++ public/corelibs/wick-engine/wickengine.js
@@ -60302,6 +60302,10 @@ Wick.Tools.Line = class extends Wick.Tool {
     });
     this.startPoint;
     this.endPoint;
+	this.hitResult = new this.paper.HitResult();
+	this.hoverPreview = new this.paper.Item({
+		insert: false
+	});
   }
 
   get doubleClickEnabled() {
@@ -60329,13 +60333,71 @@ Wick.Tools.Line = class extends Wick.Tool {
     this.path.remove();
   }
 
+  onMouseMove(e) {
+	// inherit mousemove class
+	super.onMouseMove(e); 
+
+	// remove the hover preview
+	this.hoverPreview.remove();
+
+	// find what's under the cursor
+	this.hitResult = this._updateHitResult(e);
+
+	// && !this.hitResult.item.data.isSelectionBoxGUI
+	if(this.hitResult.type === 'segment') {
+		this.hoverPreview = new this.paper.Path.Circle(this.hitResult.segment.point, 5/this.paper.view.zoom);
+		this.hoverPreview.strokeColor = 'rgba(100,100,100,0)';
+		this.hoverPreview.strokeWidth = 1.5;
+		this.hoverPreview.fillColor = 'rgba(150,0,0,0.5)';
+
+		this.startPoint = this.hitResult.segment.point;
+	}else{
+		this.startPoint = false;
+	}
+
+	this.hoverPreview.data.wickType = 'gui';
+  }
+
   onMouseDown(e) {
-    this.startPoint = e.point;
+	
+	// check if there is a hoverpoint
+	// else use e.point for startPoint
+	if(!this.startPoint)
+	this.startPoint = e.point;
   }
 
   onMouseDrag(e) {
     this.path.remove();
-    this.endPoint = e.point;
+
+	// remove the hover preview
+	this.hoverPreview.remove();
+
+	// find what's under the cursor
+	this.hitResult = this._updateHitResult(e);
+
+	// straight line
+	if(e.modifiers.shift){
+		const dy = Math.abs(this.startPoint.y - e.point.y);
+		const dx = Math.abs(this.startPoint.x - e.point.x);
+
+		this.endPoint = {
+			x: e.point.x,
+			y: this.startPoint.y
+		}
+	}else // snap to point
+	if(this.hitResult.type === 'segment') {
+		this.endPoint = this.hitResult.segment.point;
+
+		this.hoverPreview = new this.paper.Path.Circle(this.hitResult.segment.point, 5/this.paper.view.zoom);
+		this.hoverPreview.strokeColor = 'rgba(100,100,100,0)';
+		this.hoverPreview.strokeWidth = 1.5;
+		this.hoverPreview.fillColor = 
+		this.endPoint===this.startPoint?'rgba(150,50,50,0.5)':'rgba(50,50,255,0.5)';
+	}else{ // default to cursor position
+		this.endPoint = e.point;
+	}
+	this.hoverPreview.data.wickType = 'gui';
+
     this.path = new paper.Path.Line(this.startPoint, this.endPoint);
     this.path.strokeCap = 'round';
     this.path.strokeColor = this.getSetting('strokeColor').rgba;
@@ -60343,12 +60405,41 @@ Wick.Tools.Line = class extends Wick.Tool {
   }
 
   onMouseUp(e) {
+	this.hoverPreview.remove();
     this.path.remove();
-    this.addPathToProject(this.path);
-    this.fireEvent({
-      eventName: 'canvasModified',
-      actionName: 'line'
+	if(this.startPoint !== this.endPoint){
+		this.addPathToProject(this.path);
+		this.fireEvent({
+		eventName: 'canvasModified',
+		actionName: 'line'
+		});
+	}
+  }
+
+  // todo: clean this function -H.A.
+  _updateHitResult(e) {
+    var newHitResult = this.paper.project.hitTest(e.point, {
+      fill: false,
+      stroke: false,
+      curves: false,
+      segments: true,
+      handles: false,
+      tolerance: 8,
+      match: result => {
+        return result.item !== this.hoverPreview && !result.item.data.isBorder;
+      }
     });
+
+    if (!newHitResult) newHitResult = new this.paper.HitResult();
+
+    if (newHitResult.item && !newHitResult.item.data.isSelectionBoxGUI) {
+      // You can't select children of compound paths, you can only select the whole thing.
+      if (newHitResult.item.parent.className === 'CompoundPath') {
+        newHitResult.item = newHitResult.item.parent;
+      }
+    }
+
+    return newHitResult;
   }
 
 };
