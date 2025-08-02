22a577d8a77edacb9df701315ceb1421d39e83db
alani25 <alanihamzah25@gmail.com>
2025-08-02 16:43:23 -0400

    updated code in engine src

--- engine/src/tools/Line.js
+++ engine/src/tools/Line.js
@@ -18,60 +18,171 @@
  */
 
 Wick.Tools.Line = class extends Wick.Tool {
-    /**
-     *
-     */
-    constructor () {
-        super();
+  /**
+   *
+   */
+  constructor() {
+    super();
+    this.name = 'line';
+    this.path = new this.paper.Path({
+      insert: false
+    });
+    this.startPoint;
+    this.endPoint;
+	this.hitResult = new this.paper.HitResult();
+	this.hoverPreview = new this.paper.Item({
+		insert: false
+	});
+  }
 
-        this.name = 'line';
+  get doubleClickEnabled() {
+    return false;
+  }
+  /**
+   *
+   * @type {string}
+   */
 
-        this.path = new this.paper.Path({insert:false});
 
-        this.startPoint;
-        this.endPoint;
+  get cursor() {
+    return 'crosshair';
+  }
+
+  get isDrawingTool() {
+    return true;
+  }
+
+  onActivate(e) {
+    this.path.remove();
+  }
+
+  onDeactivate(e) {
+    this.path.remove();
+  }
+
+  onMouseMove(e) {
+	// inherit mousemove class
+	super.onMouseMove(e); 
+
+	// remove the hover preview
+	this.hoverPreview.remove();
+
+	// find what's under the cursor
+	this.hitResult = this._updateHitResult(e);
+
+	// && !this.hitResult.item.data.isSelectionBoxGUI
+	if(this.hitResult.type === 'segment') {
+		this.hoverPreview = new this.paper.Path.Circle(this.hitResult.segment.point, 5/this.paper.view.zoom);
+		this.hoverPreview.strokeColor = 'rgba(100,100,100,0)';
+		this.hoverPreview.strokeWidth = 1.5;
+		this.hoverPreview.fillColor = 'rgba(150,0,0,0.5)';
+
+		this.startPoint = this.hitResult.segment.point;
+	}else{
+		this.startPoint = false;
+	}
+
+	this.hoverPreview.data.wickType = 'gui';
+  }
+
+  onMouseDown(e) {
+	
+	// check if there is a hoverpoint
+	// else use e.point for startPoint
+	if(!this.startPoint)
+	this.startPoint = e.point;
+  }
+
+  onMouseDrag(e) {
+    this.path.remove();
+
+	// remove the hover preview
+	this.hoverPreview.remove();
+
+	// find what's under the cursor
+	this.hitResult = this._updateHitResult(e);
+
+	// else // snap to point
+	if(this.hitResult.type === 'segment') {
+		this.endPoint = {
+			x: this.hitResult.segment.point.x,
+			y: this.hitResult.segment.point.y
+		};
+
+		this.hoverPreview = new this.paper.Path.Circle(this.hitResult.segment.point, 5/this.paper.view.zoom);
+		this.hoverPreview.strokeColor = 'rgba(100,100,100,0)';
+		this.hoverPreview.strokeWidth = 1.5;
+		this.hoverPreview.fillColor = 
+		this.endPoint===this.startPoint?'rgba(150,50,50,0.5)':'rgba(50,50,255,0.5)';
+	}else{ // default to cursor position
+		this.endPoint = e.point;
+	}
+
+	// straight line
+	if(e.modifiers.shift){
+		const dy = Math.abs(this.startPoint.y - e.point.y);
+		const dx = Math.abs(this.startPoint.x - e.point.x);
+
+		// diagnol
+		if(dy && dx && (dy/dx)>0.5 && (dx/dy)>0.5){
+			const diff = {
+				x: this.endPoint.x-this.startPoint.x,
+				y: this.endPoint.y-this.startPoint.y
+			}
+			this.endPoint = {
+				x: this.startPoint.x + (dy+dx)/2 * diff.x/Math.abs(diff.x),
+				y: this.startPoint.y + (dy+dx)/2 * diff.y/Math.abs(diff.y)
+			}
+		}else if(dy>dx) // straight vertical
+			this.endPoint.x = this.startPoint.x;
+		else // straight horizontal
+			this.endPoint.y = this.startPoint.y;
+
+	}
+	this.hoverPreview.data.wickType = 'gui';
+
+    this.path = new paper.Path.Line(this.startPoint, this.endPoint);
+    this.path.strokeCap = 'round';
+    this.path.strokeColor = this.getSetting('strokeColor').rgba;
+    this.path.strokeWidth = this.getSetting('strokeWidth');
+  }
+
+  onMouseUp(e) {
+	this.hoverPreview.remove();
+    this.path.remove();
+	if(this.startPoint !== this.endPoint){
+		this.addPathToProject(this.path);
+		this.fireEvent({
+		eventName: 'canvasModified',
+		actionName: 'line'
+		});
+	}
+  }
+
+  // todo: clean this function -H.A.
+  _updateHitResult(e) {
+    var newHitResult = this.paper.project.hitTest(e.point, {
+      fill: false,
+      stroke: false,
+      curves: false,
+      segments: true,
+      handles: false,
+      tolerance: 8,
+      match: result => {
+        return result.item !== this.hoverPreview && !result.item.data.isBorder;
+      }
+    });
+
+    if (!newHitResult) newHitResult = new this.paper.HitResult();
+
+    if (newHitResult.item && !newHitResult.item.data.isSelectionBoxGUI) {
+      // You can't select children of compound paths, you can only select the whole thing.
+      if (newHitResult.item.parent.className === 'CompoundPath') {
+        newHitResult.item = newHitResult.item.parent;
+      }
     }
 
-    get doubleClickEnabled () {
-        return false;
-    }
+    return newHitResult;
+  }
 
-    /**
-     *
-     * @type {string}
-     */
-    get cursor () {
-        return 'crosshair';
-    }
-
-    get isDrawingTool () {
-        return true;
-    }
-
-    onActivate (e) {
-        this.path.remove();
-    }
-
-    onDeactivate (e) {
-        this.path.remove();
-    }
-
-    onMouseDown (e) {
-        this.startPoint = e.point;
-    }
-
-    onMouseDrag (e) {
-        this.path.remove();
-        this.endPoint = e.point;
-        this.path = new paper.Path.Line(this.startPoint, this.endPoint);
-        this.path.strokeCap = 'round';
-        this.path.strokeColor = this.getSetting('strokeColor').rgba;
-        this.path.strokeWidth = this.getSetting('strokeWidth');
-    }
-
-    onMouseUp (e) {
-        this.path.remove();
-        this.addPathToProject(this.path);
-        this.fireEvent({eventName: 'canvasModified', actionName: 'line'});
-    }
-}
+};
