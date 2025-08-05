455146545ece898681f2ad541b51e1bbb9e1730d
alani25 <alanihamzah25@gmail.com>
2025-08-05 16:54:12 -0400

    updated line.js in engine directory

--- engine/src/tools/Line.js
+++ engine/src/tools/Line.js
@@ -70,8 +70,8 @@ Wick.Tools.Line = class extends Wick.Tool {
 	// find what's under the cursor
 	this.hitResult = this._updateHitResult(e);
 
-	// && !this.hitResult.item.data.isSelectionBoxGUI
-	if(this.hitResult.type === 'segment') {
+	// alt/ option or command/ control to snap to nearby selection
+	if( (e.modifiers.alt || e.modifiers.command || e.modifiers.control || e.modifiers.option ) && this.hitResult.type === 'segment') {
 		this.hoverPreview = new this.paper.Path.Circle(this.hitResult.segment.point, 5/this.paper.view.zoom);
 		this.hoverPreview.strokeColor = 'rgba(100,100,100,0)';
 		this.hoverPreview.strokeWidth = 1.5;
@@ -102,8 +102,8 @@ Wick.Tools.Line = class extends Wick.Tool {
 	// find what's under the cursor
 	this.hitResult = this._updateHitResult(e);
 
-	// else // snap to point
-	if(this.hitResult.type === 'segment') {
+	// snap to point if option/ alt or command/ control
+	if( (e.modifiers.alt || e.modifiers.command || e.modifiers.control || e.modifiers.option ) && this.hitResult.type === 'segment') {
 		this.endPoint = {
 			x: this.hitResult.segment.point.x,
 			y: this.hitResult.segment.point.y
@@ -171,7 +171,7 @@ Wick.Tools.Line = class extends Wick.Tool {
       curves: false,
       segments: true,
       handles: false,
-      tolerance: 8,
+      tolerance: 10,
       match: result => {
         return result.item !== this.hoverPreview && !result.item.data.isBorder;
       }
