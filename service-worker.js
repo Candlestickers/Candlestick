8f4402cc2a596b96557a2f7e42041c070d31e7e5
alani25 <alanihamzah25@gmail.com>
2025-08-22 21:40:55 -0400

    deploy prod

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Candlestick/precache-manifest.04c9c1a71e2a8a8e13238c4c9f54cb41.js"
+  "/Candlestick/test/precache-manifest.a7595e963e19c7ee8251833f4f7110ad.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Candlestick/index.html", {
+workbox.routing.registerNavigationRoute("/Candlestick/test/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
