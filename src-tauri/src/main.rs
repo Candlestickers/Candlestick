fbab293ae3dba5b271ac1d441ee5a5c044e2e354
alani25 <alanihamzah25@gmail.com>
2025-11-03 16:56:22 -0500

    updated tauri config to allow global variables & preview window & used tauri webview API

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
-  app_lib::run();
+    tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
