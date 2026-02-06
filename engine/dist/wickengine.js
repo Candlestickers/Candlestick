b919c306eafb78db08e62478da4904752a644e6a
alani25 <alanihamzah25@gmail.com>
2026-02-06 11:43:59 -0500

    switching MP4 export to tauri rust in app version

--- engine/dist/wickengine.js
+++ engine/dist/wickengine.js
@@ -1,5 +1,5 @@
 /*Wick Engine https://github.com/Wicklets/wick-engine*/
-var WICK_ENGINE_BUILD_VERSION = "2025.11.7.10.54.45";
+var WICK_ENGINE_BUILD_VERSION = "2026.2.3.12.19.24";
 /*!
  * Paper.js v0.12.4 - The Swiss Army Knife of Vector Graphics Scripting.
  * http://paperjs.org/
