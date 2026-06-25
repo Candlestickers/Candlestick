143c30056c04d2eb9f837fcf2765e3b7569daecf
alani25 <alanihamzah25@gmail.com>
2026-06-24 22:19:37 -0400

    fixed final sass file error

--- public/corelibs/wick-engine/wickengine.js
+++ public/corelibs/wick-engine/wickengine.js
@@ -1,5 +1,5 @@
 /*Wick Engine https://github.com/Wicklets/wick-engine*/
-var WICK_ENGINE_BUILD_VERSION = "2026.6.24.21.42.55";
+var WICK_ENGINE_BUILD_VERSION = "2026.6.24.22.18.26";
 /*!
  * Paper.js v0.12.4 - The Swiss Army Knife of Vector Graphics Scripting.
  * http://paperjs.org/
