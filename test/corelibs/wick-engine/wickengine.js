d9204ec46a14ac9f47c7f0fc0f3050942d2bb8b3
alani25 <alanihamzah25@gmail.com>
2026-07-02 21:51:47 -0400

    deploy test

--- test/corelibs/wick-engine/wickengine.js
+++ test/corelibs/wick-engine/wickengine.js
@@ -1,5 +1,5 @@
 /*Wick Engine https://github.com/Wicklets/wick-engine*/
-var WICK_ENGINE_BUILD_VERSION = "2026.7.1.21.36.53";
+var WICK_ENGINE_BUILD_VERSION = "2026.7.2.21.45.23";
 /*!
  * Paper.js v0.12.4 - The Swiss Army Knife of Vector Graphics Scripting.
  * http://paperjs.org/
