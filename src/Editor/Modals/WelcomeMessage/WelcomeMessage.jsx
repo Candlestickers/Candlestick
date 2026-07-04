bfc6f4a3c188ad28b9aef5cfffc7bd10d153fcc4
alani25 <alanihamzah25@gmail.com>
2026-07-04 13:18:06 -0400

    updated version number

--- src/Editor/Modals/WelcomeMessage/WelcomeMessage.jsx
+++ src/Editor/Modals/WelcomeMessage/WelcomeMessage.jsx
@@ -36,10 +36,10 @@ class WelcomeModal extends Component {
         super();
         this.forumPost = "https://url.candlestickers.app/updates";
         this.updates = [
-            "Updated Color Picker",
-            "PDF Import and Export",
-            "Pasting Images from Clipboard",
-            "Various Bug/Crash Fixes"
+            "Updated MP4 Exporter",
+            // "PDF Import and Export",
+            // "Pasting Images from Clipboard",
+            // "Various Bug/Crash Fixes"
         ]
 
         /*let t1 = ["Guy de Bree", "Dimp", "Hyun's Dojo"]
