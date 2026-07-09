41f66374ef6f61ced6345388a754885d367e3360
alani25 <alanihamzah25@gmail.com>
2026-07-09 15:08:00 -0400

    important android build fixes

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b01bb80b64916f502a3033afdb4567227ef2abcc3b938889f02283aefa780fbe
-size 37
+oid sha256:de486adaddcaae09a19b2937f38599504abd1fee45a66192b6128e053ac907f4
+size 233
