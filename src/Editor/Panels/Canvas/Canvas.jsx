9f14c4becdb45859ad00a1fd4eb7637712823216
alani25 <alanihamzah25@gmail.com>
2026-07-11 00:26:14 -0400

    updated react-dnd; HOC to hook api

--- src/Editor/Panels/Canvas/Canvas.jsx
+++ src/Editor/Panels/Canvas/Canvas.jsx
@@ -17,8 +17,8 @@
  * along with Wick Editor.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import React, { Component } from 'react';
-import { DropTarget } from 'react-dnd';
+import React, { Component, useRef, useEffect } from 'react';
+import { useDrop } from 'react-dnd';
 import DragDropTypes from 'Editor/DragDropTypes.js';
 
 import './_canvas.scss';
@@ -65,49 +65,50 @@ class Canvas extends Component {
   }
 
   render() {
-    const { connectDropTarget, isOver } = this.props;
+    const { dropRef, isOver } = this.props;
 
-    return connectDropTarget (
-      <div id="canvas-container-wrapper" style={{width:"100%", height:"100%"}} aria-label="Canvas">
+    return (
+      <div id="canvas-container-wrapper" style={{width:"100%", height:"100%"}} aria-label="Canvas" ref={dropRef}>
         { isOver && <div className="drag-drop-overlay" /> }
         <div id="wick-canvas-container" ref={this.canvasContainer}></div>
       </div>
-    )
+    );
   }
 }
 
-// react-dnd drag and drop target params
-const canvasTarget = {
-  drop(props, monitor, component) {
-    const dropLocation = monitor.getClientOffset();
-    let draggedItem = monitor.getItem();
-    if(draggedItem.files && draggedItem.files.length > 0) {
-      // Dropped a file from native filesystem
-      var file = draggedItem.files[0];
-      var name = file.name;
-      if(name.endsWith('.wick')) {
-        // Wick Project (.wick file)
-        props.importProjectAsWickFile(file);
-      } else if(file.type === 'video/mp4' || name.endsWith('.mp4') ||
-                file.type === 'application/pdf' || name.endsWith('.pdf')) {
-        // MP4/PDF → open as new project
-        props.openProjectFile(file);
+function CanvasDrop(props) {
+  const propsRef = useRef(props);
+  useEffect(() => { propsRef.current = props; });
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: DragDropTypes.CANVAS,
+    drop(item, monitor) {
+      const p = propsRef.current;
+      const dropLocation = monitor.getClientOffset();
+      if (item.files && item.files.length > 0) {
+        // Dropped a file from native filesystem
+        var file = item.files[0];
+        var name = file.name;
+        if (name.endsWith('.wick')) {
+          // Wick Project (.wick file)
+          p.importProjectAsWickFile(file);
+        } else if (file.type === 'video/mp4' || name.endsWith('.mp4') ||
+                   file.type === 'application/pdf' || name.endsWith('.pdf')) {
+          // MP4/PDF → open as new project
+          p.openProjectFile(file);
+        } else {
+          // Assets (images, sounds, etc)
+          p.createAssets(item.files, [], {create: true, location: dropLocation});
+        }
       } else {
-        // Assets (images, sounds, etc)
-        props.createAssets(draggedItem.files, [], {create: true, location: dropLocation});
+        // Dropped an asset from the asset library
+        p.createImageFromAsset(item.uuid, dropLocation.x, dropLocation.y);
       }
-    } else {
-      // Dropped an asset from the asset library
-      props.createImageFromAsset(draggedItem.uuid, dropLocation.x, dropLocation.y);
-    }
-  }
+    },
+    collect: monitor => ({ isOver: monitor.isOver() }),
+  }));
+
+  return <Canvas {...props} isOver={isOver} dropRef={drop} />;
 }
 
-function collect(connect, monitor) {
-  return {
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver(),
-  };
-}
-
-export default DropTarget(DragDropTypes.CANVAS, canvasTarget, collect)(Canvas);
+export default CanvasDrop;
