9f550189de280b05f15301d819f74f9bd07e87fc
hobbes6-2 <shreya.deb@gmail.com>
2026-08-20 15:01:08 +0530

    finished install(); todo: uninstall(), test a lot

--- engine/src/WickPM.js
+++ engine/src/WickPM.js
@@ -33,37 +33,59 @@ Wick.WickPM = class {
      * @param {string} version - (OPTIONAL) The version of the package. Defaults to latest.
      */
     install(pkg, imports, version = 'latest'){
-        pkg.toLowerCase();
-        console.log(pkg, this.pkgs)
-        if (this.pkgs.some(pkgy => pkgy.pkg == pkg)) {console.log('Package already exists, exiting installer'); return;}
-        version.replaceAll('.x', '');
-        let pkgStr = 'https://' + 'esm.sh/' + pkg + '@' + version;
+        
+        // format params
+        pkg = pkg.toLowerCase();
+        version = version.replaceAll('.x', '');
+        imports = imports.replaceAll(' ', '');
+        imports = imports.replaceAll('    ', '');
+        console.log("pkg and pkgs:", pkg, this.pkgs)
+
+        // check if package is installed
+        if (this.pkgs.some(pkgy => pkgy.pkg == pkg)) {
+            if (this.pkgs.some(pkgy => pkgy.imports == imports)){
+                console.log('Package already exists, exiting installer');
+                return;
+            }
+        }
+
+        // generate some different strings for readability
+        let pkgStr = 'https://esm.sh/' + pkg + '@' + version;
         let mainStr = '\n import ' + imports + ' from "' + pkgStr + '";';
         let importLoader = '', importAccessor = 'window.editor.project.WickPM.imports.';
+
+        // check if imports are named or default
         if (!imports.includes('{')) importLoader = '\n' + importAccessor + imports + ' = ' + imports + ';';
         else {
-            // remove whitespace and braces
-            imports.replaceAll(' ', '');
-            imports.replaceAll('    ', '');
-            imports.replaceAll('{', '');
-            imports.replaceAll('}', '');
-            const importArr = imports.split(',');
+            // remove braces
+            imports = imports.replaceAll('{', '');
+            imports = imports.replaceAll('}', '');
+
+            let importArr = imports.split(',');
+            
+            // remove any empty elements
+            importArr = importArr.filter(imp => imp != "");
+
+            // generate the strings
             importArr.forEach((Import) => {
-                console.log(importArr, Import, imports)
                 importLoader += '\n' + importAccessor + pkg + ' = ' + '{...' + importAccessor + pkg + ',' + Import + ':' + Import + '}' + ';';
             })
-            importLoader = '\n' + importAccessor + pkg + ' = ' + imports + ';';
         }
+
+        // attach to a script tag
         let txt = this.loader && this.loader.textContent || '';
         if(this.loader) this.loader.remove();
         this.loader = document.createElement('script');
         this.loader.type = 'module';
-        this.loader.id = "pkg-loader";
+        this.loader.id = "pkg-loader"; // this is for anyone who wants to read the script tag from elsewhere
         document.head.appendChild(this.loader);
         this.loader.textContent = txt + mainStr + importLoader;
+
+        // add to our packages array
         this.pkgs.push({pkg: pkg, imports: imports, version: version});
     }
 
+    // todo: make uninstall run at all
     /**
      * Uninstalls a package from the project.
      * @param {string} pkg - The package to remove
