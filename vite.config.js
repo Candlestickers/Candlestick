89e8ea79d61ca0d888f627bb2d77a5c2ab19fd7e
hobbes6-2 <shreya.deb@gmail.com>
2026-09-10 17:03:53 +0530

    audit fix and browserslist

--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,19 @@ export default defineConfig({
   },
   build: {
     outDir: "build",
+    /* I think this is the same as the browserslist config; for reference it's here: 
+    "browserslist": [
+    ">0.2%",
+    "not dead",
+    "not ie <= 11",
+    "not op_mini all"],*/
+    target: [
+      'chrome109',
+      'ios15.6',
+      'safari15.6',
+      'edge149',
+      'firefox121',
+      'opera80'
+    ]
   },
 });
